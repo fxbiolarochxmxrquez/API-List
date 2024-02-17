1a92e5cecf0dc0c17f342d6245c44efc729f3053
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-02-17 12:26:38 -0600

    Initial Commit

---
+++ API List/API_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ListApp.swift
+//  API List
+//
+//  Created by Fabiola Rocha Marquez on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct API_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
