97ffdde348f73034619822d68e934e77f553e93d
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-02-17 22:29:22 -0600

    Setup EntryView

---
+++ API List/EntryView.swift
@@ -0,0 +1,18 @@
+//
+//  EntryView.swift
+//  API List
+//
+//  Created by Fabiola Rocha Marquez on 2/17/24.
+//
+
+import SwiftUI
+
+struct EntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EntryView()
+}
